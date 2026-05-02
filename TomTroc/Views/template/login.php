adbe5358a822e6f7da935ce483b832ba8c74f07e
Matthias Beill <matthias.beill@laposte.net>
2026-05-02 04:34:01 +0200

    feat: added login page

--- TomTroc/Views/template/login.php
+++ TomTroc/Views/template/login.php
@@ -1,14 +1,18 @@
 <?php
 ?>
 
-<div class="signup-form">
-    <form action="index.php?action=registerUser" method="post" class="foldedCorner">
-        <h2>Inscription</h2>
-        <div class="formGrid">
+<div class="login">
+    <form action="index.php?action=login" method="post" class="foldedCorner">
+        <div class="login-form">
+            <h2>Connexion</h2>
             <label for="login">Login</label>
             <input type="text" name="login" id="login" required>
             <label for="password">Mot de passe</label>
             <input type="password" name="password" id="password" required>
-            <button class="submit">S'inscrire</button>
+            <a class="btn btn-primary">S'inscrire</a>
         </div>
+        
     </form>
+    <div class="login-media">
+            <img src="assets/login/logImg.svg" alt="signup" class="signup-image">
+        </div>
