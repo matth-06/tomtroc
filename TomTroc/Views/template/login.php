bdfaa1dbaaff9fa6a15dca262f7df9be2219581a
Matthias Beill <matthias.beill@laposte.net>
2026-05-02 04:51:35 +0200

    style: modified style of login page

--- TomTroc/Views/template/login.php
+++ TomTroc/Views/template/login.php
@@ -5,11 +5,11 @@
     <form action="index.php?action=login" method="post" class="foldedCorner">
         <div class="login-form">
             <h2>Connexion</h2>
-            <label for="login">Login</label>
-            <input type="text" name="login" id="login" required>
+            <label for="login">Adresse email</label>
+            <input type="email" name="login" id="login" required>
             <label for="password">Mot de passe</label>
             <input type="password" name="password" id="password" required>
-            <a class="btn btn-primary">S'inscrire</a>
+            <a class="btn btn-primary login-btn">S'inscrire</a>
         </div>
         
     </form>
